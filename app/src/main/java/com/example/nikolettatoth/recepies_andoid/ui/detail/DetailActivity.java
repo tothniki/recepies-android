bb9cb5bfe534c8d7c7d284d585a62d239fd61fbc
Nikoletta Toth <tothniki.ntt@gmail.com>
2018-04-16 01:48:26 +0200

    Create ui.detail DetailActivity/Presenter/Screen basic code.

--- app/src/main/java/com/example/nikolettatoth/recepies_andoid/ui/detail/DetailActivity.java
+++ app/src/main/java/com/example/nikolettatoth/recepies_andoid/ui/detail/DetailActivity.java
@@ -1,4 +1,23 @@
 package com.example.nikolettatoth.recepies_andoid.ui.detail;
 
-public class DetailActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity implements DetailScreen{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 }
--- app/src/main/java/com/example/nikolettatoth/recepies_andoid/ui/detail/DetailPresenter.java
+++ app/src/main/java/com/example/nikolettatoth/recepies_andoid/ui/detail/DetailPresenter.java
@@ -1,4 +1,16 @@
 package com.example.nikolettatoth.recepies_andoid.ui.detail;
 
-public class DetailPresenter {
+import com.example.nikolettatoth.recepies_andoid.ui.Presenter;
+
+public class DetailPresenter extends Presenter<DetailScreen> {
+
+    @Override
+    public void attachScreen(DetailScreen screen) {
+        super.attachScreen(screen);
+    }
+
+    @Override
+    public void detachScreen() {
+        super.detachScreen();
+    }
 }
