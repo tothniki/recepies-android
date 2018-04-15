e8a706957ead767327db9efbc0615fc57e084ff4
Nikoletta Toth <tothniki.ntt@gmail.com>
2018-04-16 01:54:31 +0200

    Create ui.about AboutActivity/Presenter/Screen basic code.

--- app/src/main/java/com/example/nikolettatoth/recepies_andoid/ui/about/AboutActivity.java
+++ app/src/main/java/com/example/nikolettatoth/recepies_andoid/ui/about/AboutActivity.java
@@ -1,4 +1,26 @@
 package com.example.nikolettatoth.recepies_andoid.ui.about;
 
-public class AboutActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.nikolettatoth.recepies_andoid.R;
+
+public class AboutActivity extends AppCompatActivity implements AboutScreen {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 }
